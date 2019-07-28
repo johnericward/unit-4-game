8372e7a8e6b65b6ad9aaae39896cf2d984af0f8e
johnericward <johnericward@gmail.com>
2019-07-28 19:58:34 -0400

    worked out sizing of field and made characters land on left and right sides

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,47 +22,78 @@ $(document).ready(function() {
         // var bulbasaurPic = $('#bulbasaur').append('<img id="bulbasaur" src="assets/images/bulbasaur.png" />');
     
     var pikachu = $("#pikachu");
+    var pikachu2 = $("#pikachu2");
+    var pikachu3 = $("#pikachu3");
     var squirtle = $("#squirtle");
+    var squirtle2 = $("#squirtle2");
+    var squirtle3 = $("#squirtle3");
     var charmander = $("#charmander");
+    var charmander2 = $("#charmander2");
+    var charmander3 = $("#charmander3");
     var bulbasaur = $("#bulbasaur");
+    var bulbasaur2 = $("#bulbasaur2");
+    var bulbasaur3 = $("#bulbasaur3");
 
-    var charcterChoices = [pikachu, squirtle, charmander, bulbasaur];
+    var charcterChoices = [pikachu2, squirtle2, charmander2, bulbasaur2];
 
     var userCharacter;
     var cpu;
     var hp;
     var damageGiven;
     
-
-
+    // left side of field
+    pikachu2.animate({ opacity: "0" });
+    squirtle2.animate({ opacity: "0" });
+    charmander2.animate({ opacity: "0" });
+    bulbasaur2.animate({ opacity: "0" });
+    // right side of field
+    pikachu3.animate({ opacity: "0" });
+    squirtle3.animate({ opacity: "0" });
+    charmander3.animate({ opacity: "0" });
+    bulbasaur3.animate({ opacity: "0" });
     
-  
-        $("#pikachu").on("click", function() {
-            pikachu.animate({ opacity: "0.05" });
+    
 
+
+        $("#pikachu").on("click", function() {
+            pikachu.animate({ opacity: "0.00" });
             console.log("pika works");
+            pikachu2.animate({ opacity: "1" });
+            pikachu3.animate({ opacity: "1" });
         });
     
         $("#squirtle").on("click", function() {
-            squirtle.animate({ opacity: "0.05" });
+            squirtle.animate({ opacity: "0.00" });
             console.log("squi works");
+            squirtle2.animate({ opacity: "1" });
+            squirtle3.animate({ opacity: "1" });
         });
     
         $("#charmander").on("click", function() {
-            charmander.animate({ opacity: "0.05" });
+            charmander.animate({ opacity: "0.00" });
             console.log("char works");
+            charmander2.animate({ opacity: "1" });
+            charmander3.animate({ opacity: "1" });
         });
     
         $("#bulbasaur").on("click", function() {
-            bulbasaur.animate({ opacity: "0.05" });
+            bulbasaur.animate({ opacity: "0.00" });
             console.log("bulba works");
+            bulbasaur2.animate({ opacity: "1" });
+            bulbasaur3.animate({ opacity: "1" });
         });
     
     
-    console.log("if then works!");
+   
 
     
 
 
 // ========================= ready statement ending vvv ============================
         });
+
+       
+
+
+   
+   
